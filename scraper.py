3b67828876935ddbee0ec566e100d5b94f7926e2
lpmi-13 <leskis@gmail.com>
2016-10-02 17:23:08 +0100

    add all times for all distances in array for both Men and Women

--- scraper.py
+++ scraper.py
@@ -14,12 +14,45 @@ recordtable = recordsoup.find('table', class_="multicol")
 
 record_links = recordtable.find_all('a')
 
+def getNationality(url):
+    Georgian = re.search('Georgia', url)
+    if (Georgian):
+	return 'Georgian'
+    else:
+	return re.search('List_of_(.+?)_records', url).group(1)
+
 for link in record_links:
     national_link = link.get('href', None)
     national_record_page = urllib.urlopen(base_url + national_link).read()
     starting_el = BeautifulSoup(national_record_page, 'html.parser')
     table = starting_el.find('span', {'id':'Men'})
     mens_table = table.find_next('table', class_='wikitable')
-    race_distance = mens_table.find('a', href='/wiki/100_metres')
-    race_time = race_distance.find_parent('td').find_next_sibling()
-    print re.match('\S+', race_time.text).group(0)
+
+    table = starting_el.find('span', {'id':'Women'})
+    womens_table = table.find_next('table', class_='wikitable')
+
+    print getNationality(national_link)
+    for distance in race_array:
+        men_race_distance = mens_table.find('a', href='/wiki/'+ distance)
+	women_race_distance = womens_table.find('a', href='/wiki/'+distance)
+
+	print distance
+	print 'Men'
+        try:
+	    men_race_time = men_race_distance.find_parent('td').find_next_sibling()
+            try:
+                print re.match('\S+', men_race_time.text).group(0)
+            except:
+                print 'no time found'
+	except:
+	    print 'no time found'
+
+	print 'Women'
+	try:
+	    women_race_time = women_race_distance.find_parent('td').find_next_sibling()
+	    try:
+		print re.match('\S+', women_race_time.text).group(0)
+	    except:
+		print 'no time found'
+	except:
+	    print 'no time found'
